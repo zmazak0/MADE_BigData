54641786b2e09d721fedcca497950cdc6d5273de
zmazak0 <55308199+zmazak0@users.noreply.github.com>
2021-10-28 00:13:50 +0300

    HW3

--- HW3/build.sbt
+++ HW3/build.sbt
@@ -5,8 +5,6 @@ version := "0.1"
 scalaVersion := "2.13.6"
 
 libraryDependencies  ++= Seq(
-  // Last stable release
-  "org.scalanlp" %% "breeze" % "1.2",
-
-  "org.scalanlp" %% "breeze-viz" % "1.2"
+  "org.scalanlp" %% "breeze-viz" % "1.2",
+  "org.scalanlp" %% "breeze" % "1.2"
 )
--- HW3/src/main/scala/Main.scala
+++ HW3/src/main/scala/Main.scala
@@ -3,7 +3,6 @@ import breeze.numerics.{pow, sqrt}
 import java.io.{File, FileWriter}
 
 
-
 class LinearRegression {
   def fit(X: DenseMatrix[Double], y: DenseVector[Double],
           nIterations: Int, learnRate: Double): (DenseVector[Double], Double) = {
@@ -41,29 +40,36 @@ class io {
     (xTrain, yTrain, xTest, yTest)
   }
 
-  def writePredictions(Ypredict: DenseVector[Double], outPath: String): Unit = {
-    csvwrite(new File(outPath), separator = ',', mat = Ypredict.toDenseMatrix)
+  def writePredictions(Predict: DenseVector[Double], outPath: String): Unit = {
+    csvwrite(new File(outPath), separator = ',', mat = Predict.toDenseMatrix)
   }
 }
 
 
 object Main {
   def main(args: Array[String]): Unit = {
+    val writer = new FileWriter("C:\\Users\\Zaven\\Desktop\\HW-3\\data\\logs.txt")
+
     val (targetPath, dataPath, outPath) = (args(0), args(1), args(2))
-    val (nIter, learnRate) = (args(3).toInt, args(4).toDouble) // 1_000 .01
+    val (nIterations, learningRate) = (args(3).toInt, args(4).toDouble) // 1_000 .01
     val (io, model) = (new io, new LinearRegression)
 
     val (xTrain, yTrain, xTest, yTest) = io.readAndSplit(dataPath, targetPath)
-    println(s"Successfully read and split data, ${xTrain.rows + xTest.rows} rows, ${xTrain.cols} columns")
-    val (weights, bias) = model.fit(xTrain, yTrain, nIter, learnRate)
-    println(s"Linear Regression fitted. \nweights: ${weights} \nbias: ${bias}")
-    val yPred = model.predict(xTest, weights, bias)
-    println(s"RMSE on validation dataset: ${model.RMSE(yTest, yPred)}")
-    io.writePredictions(yPred, outPath)
-    println(s"Predictions are saved to: ${outPath}")
-    val writer = new FileWriter("C:\\Users\\Zaven\\Desktop\\HW-3\\data\\RMSE.txt")
+    println(s"Successfully read and split data, ${xTrain.rows + xTest.rows} rows, ${xTrain.cols} columns.")
+    writer.write(s"Successfully read and split data, ${xTrain.rows + xTest.rows} rows, ${xTrain.cols} columns.\n")
 
-    writer.write(s"RMSE on validation dataset = ${model.RMSE(yTest, yPred)} ")
+    val (weights, bias) = model.fit(xTrain, yTrain, nIterations, learningRate)
+    println(s"Linear Regression fitted.")
+    writer.write(s"Linear Regression fitted.")
+
+    val yPredict = model.predict(xTest, weights, bias)
+    println(s"RMSE on validation dataset: ${model.RMSE(yTest, yPredict)}.")
+    writer.write(s"RMSE on validation dataset: ${model.RMSE(yTest, yPredict)}.\n")
+
+    io.writePredictions(yPredict, outPath)
+    println(s"Predictions are saved to: ${outPath}")
+    writer.write(s"Predictions are saved to: ${outPath}\n")
+
     writer.close()
   }
 }
